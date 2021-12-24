472566fd4f1ead54ccf14659cd6b51541869c085
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2021-12-24 16:55:16 +0300

    add url exceptions

--- config/urls.py
+++ config/urls.py
@@ -14,8 +14,25 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf import settings
+from django.urls import path, include
+from django.views import defaults as default_views
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(settings.ADMIN_URL, admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path('400/', default_views.bad_request, kwargs={'exception': Exception('Bad Request!')}),
+        path('403/', default_views.permission_denied, kwargs={'exception': Exception('Permission Denied')}),
+        path('404/', default_views.page_not_found, kwargs={'exception': Exception('Page not Found')}),
+        path('500/', default_views.server_error),
+    ]
+    if 'debug_toolbar' in settings.INSTALLED_APPS:
+        import debug_toolbar
+
+        urlpatterns = [
+                          path('__debug__/', include(debug_toolbar.urls)),
+                      ] + urlpatterns
