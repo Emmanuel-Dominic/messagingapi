7a623b2e6d336c181d20999308654ce261beb627
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2021-12-24 17:02:28 +0300

    add models file

--- message/models.py
+++ message/models.py
@@ -1,24 +1,83 @@
+import os
 import uuid
-from django.db import models
+
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
-DEFAULT_AVATAR_URL="https://firebasestorage.googleapis.com/v0/b/messaging-a9715.appspot.com/o/avatars%2Fdefault.png?alt=media&token=6ad7965e-9653-493f-b6c8-364d17594356"
 # Create your models here.
+class ObjectTracking(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+        ordering = ('-created_at',)
+
+
+class ObjectManger(models.Manager):
+
+    def filter_by_instance(self, instance):
+        content_type = ContentType.objects.get_for_model(instance.__class__)
+        obj_id = instance.id
+        return super(ObjectManger, self).filter(content_type=content_type,
+                                                object_id=obj_id)
+
+
+class UserMessage(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True,
+                          editable=False)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sender")
+    body = models.TextField(null=True, blank=True)
+    body_type = models.CharField(max_length=25, choices=(('TEXT', 'text'), ('VIDEO', 'video'), ('AUDIO', 'audio'),),
+                                 blank=False, default='text')
+    msg_type = models.CharField(max_length=25, choices=(('DEFAULT', 'default'),
+                                                        ('COMMENT', 'comment'),),
+                                blank=False, default='default')
+
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE,
+                                     limit_choices_to={"model__in": ("UserProfile", "Club")},
+                                     related_name="sender")
+    object_id = models.UUIDField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(auto_now=True)
+
+    objects = ObjectManger()
+
+    def __str__(self):
+        return str(self.body)
+
+
+DEFAULT_AVATAR_URL = os.getenv("DEFAULT_AVATAR_URL")
+
+
 class UserProfile(models.Model):
-    id = models.UUIDField(default=uuid.uuid4,  unique=True, primary_key=True, editable=False)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.URLField(max_length=200, blank=True, default=DEFAULT_AVATAR_URL)
-    about = models.TextField(max_length=150, blank=True, default='Here I am using the messaging app!')
+    about = models.TextField(max_length=150, blank=True,
+                             default='Here I am using the messaging app!')
     is_online = models.BooleanField(default=False, blank=True)
     is_verified = models.BooleanField(default=False, blank=True)
 
     def __str__(self):
         return str(self.user.username)
+
+    @property
+    def messages(self):
+        return UserMessage.objects.filter_by_instance(self)
 
 
 @receiver(post_save, sender=User)
@@ -28,11 +87,17 @@ def create_user_profile(sender, instance, created, **kwargs):
 
 
 class Club(models.Model):
-    id = models.UUIDField(default=uuid.uuid4,  unique=True, primary_key=True, editable=False)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
     owner = models.ForeignKey(User, on_delete=models.PROTECT)
     title = models.CharField(max_length=50, null=True)
     about = models.TextField(max_length=200, blank=True, default="Let's talk business!")
+
+    # messages = GenericRelation(UserMessage , related_query_name='club')
+
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         unique_together = ('owner', 'title',)
@@ -40,26 +105,22 @@ class Club(models.Model):
     def __str__(self):
         return str(self.title)
 
+    @property
+    def messages(self):
+        return UserMessage.objects.filter_by_instance(self)
+
+    @property
+    def clubusers(self):
+        club = ClubUser.objects.filter(club=self.id)
+        return UserProfile.objects.filter(id__in=club.values_list('user'))
+
 
 class ClubUser(models.Model):
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     club = models.ForeignKey(Club, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         unique_together = ('user', 'club',)
-
-
-class UserMessage(models.Model):
-    id = models.UUIDField(default=uuid.uuid4,  unique=True, primary_key=True, editable=False)
-    sender = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name="sender")
-    reciever_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, related_name="sender")
-    reciever_id = models.PositiveIntegerField()
-    reciever = GenericForeignKey('reciever_type', 'reciever_id')
-    body = models.TextField(null=True, blank=True)
-    body_type = models.CharField(max_length=25, choices=(('TEXT', 'text'), ('VIDEO', 'video'), ('AUDIO', 'audio'),), blank=False, default='text')
-    msg_type = models.CharField(max_length=25, choices=(('DEFAULT', 'default'), ('COMMENT', 'comment'),), blank=False, default='default')
-    updated_at = models.DateTimeField(auto_now=True)
-    timestamp = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return str(self.body)
